ee4a0cae05d848eb9922aa24d117b35cca6be5c5
That Charming BOLE <165321522+YtCharmyngeBole@users.noreply.github.com>
2024-04-07 19:41:13 +0700

    Remove astro/prefetch as dependency, use builtin instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
 import { h } from "hastscript";
@@ -15,10 +14,10 @@ import { SITE } from "./src/config.ts";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.url,
+  prefetch: true,
   integrations: [
     tailwind({ applyBaseStyles: false, nesting: true }),
     sitemap(),
-    prefetch(),
     partytown(),
     mdx(),
   ],
