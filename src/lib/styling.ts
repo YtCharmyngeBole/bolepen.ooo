061027e4aac8d6a2480a84f79029faaf32064f96
That Charming BOLE <165321522+YtCharmyngeBole@users.noreply.github.com>
2024-04-10 15:00:17 +0700

    Refactor styling configuration script

--- src/lib/styling.ts
+++ src/lib/styling.ts
@@ -1,3 +1,7 @@
+/**
+ * Utility functions for generating CSS values,
+ * especially useful for Tailwind CSS.
+ */
 import _ from "lodash";
 
 /**
@@ -36,19 +40,54 @@ export function oklch(
 }
 
 /**
- * Generate an OKLCH palette based on the given chroma and hue.
+ * Format OKLAB color value suitable for Tailwind CSS.
  */
-export function oklchPalette(
-  c: number,
-  h: number,
-  ls: number[] = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95],
-): Record<string, string> {
-  return _.fromPairs(_.map(ls, (l) => [valueFromLuminance(l), oklch(l, c, h)]));
+export function oklab(
+  l: number | string,
+  a: number | string,
+  b: number | string,
+): string {
+  return `oklab(${l}% ${a} ${b} / <alpha-value>)`;
 }
+
+const luminancesPresets = {
+  standard: [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95],
+  extended: [
+    5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
+  ],
+} as Record<string, number[]>;
 
 function valueFromLuminance(l: number): string {
   if (0 <= l && l <= 100) {
     return ((100 - l) * 10).toFixed();
   }
   throw new Error(`Invalid luminance value: ${l}`);
+}
+
+/**
+ * Generate an OKLCH palette based on the given chroma and hue.
+ */
+export function oklchPalette(
+  c: number,
+  h: number,
+  ls: keyof typeof luminancesPresets | number[] = "standard",
+): Record<string, string> {
+  const luminances = typeof ls === "string" ? luminancesPresets[ls] : ls;
+  return _.fromPairs(
+    _.map(luminances, (l) => [valueFromLuminance(l), oklch(l, c, h)]),
+  );
+}
+
+/**
+ * Generate an OKLCH palette based on the given chroma and hue.
+ */
+export function oklabPalette(
+  a: number,
+  b: number,
+  ls: keyof typeof luminancesPresets | number[] = "standard",
+): Record<string, string> {
+  const luminances = typeof ls === "string" ? luminancesPresets[ls] : ls;
+  return _.fromPairs(
+    _.map(luminances, (l) => [valueFromLuminance(l), oklab(l, a, b)]),
+  );
 }
