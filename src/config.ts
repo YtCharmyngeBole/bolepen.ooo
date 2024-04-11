4935f23d286f9fdafa7e32bf700239bf1f1847a2
That Charming BOLE <165321522+YtCharmyngeBole@users.noreply.github.com>
2024-04-12 03:20:33 +0700

    Replace ogImage

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 import type { SocialObject } from "./helpers/types.ts";
-import ogImage from "./images/pauline-loroy-U3aF7hgUSrk-unsplash.jpg";
+import ogImage from "./images/ogimage.png";
 
 export const SITE = {
   author: "That Charming BOLE",
