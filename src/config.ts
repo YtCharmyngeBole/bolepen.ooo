154cb01337b6ef4b93843233e88fabfa8c12ee05
That Charming BOLE <165321522+YtCharmyngeBole@users.noreply.github.com>
2024-04-21 17:17:19 +0700

    Add social links label text

--- src/config.ts
+++ src/config.ts
@@ -11,73 +11,37 @@ export const SITE = {
   startYear: 2023,
 } as const;
 
-export const SOCIALS_SHORT: SocialObject[] = [
+export const SOCIALS: SocialObject[] = [
   {
-    name: "Me on Facebook",
-    icon: "facebook",
+    name: "Facebook",
     href: "https://facebook.com/YtCharmyngeBole",
   },
   {
-    name: "Me on Instagram",
-    icon: "instagram",
+    name: "Instagram",
     href: "https://instagram.com/YtCharmyngeBole",
   },
   {
-    name: "Me on Bluesky",
-    icon: "bluesky",
-    href: "https://bsky.app/profile/me.bolepen.ooo",
-  },
-  {
-    name: "Me on Twitter",
-    icon: "twitter",
-    href: "https://twitter.com/YtCharmyngeBole",
-  },
-  {
-    name: "See all links",
-    icon: "ellipsis",
-    href: "/links",
-  },
-];
-
-export const SOCIALS_FULL: SocialObject[] = [
-  {
-    name: "Me on Facebook",
-    icon: "facebook",
-    href: "https://facebook.com/YtCharmyngeBole",
-  },
-  {
-    name: "Me on Instagram",
-    icon: "instagram",
-    href: "https://instagram.com/YtCharmyngeBole",
-  },
-  {
-    name: "Me on Mastodon",
-    icon: "mastodon",
+    name: "Mastodon",
     href: "https://furry.engineer/@YtCharmyngeBole",
   },
   {
-    name: "Me on Discord",
-    icon: "discord",
+    name: "Discord",
     href: "https://discordapp.com/users/1218406319124512821",
   },
   {
-    name: "Me on Bluesky",
-    icon: "bluesky",
+    name: "Bluesky",
     href: "https://bsky.app/profile/me.bolepen.ooo",
   },
   {
-    name: "Me on Twitter",
-    icon: "twitter",
+    name: "Twitter",
     href: "https://twitter.com/YtCharmyngeBole",
   },
   {
-    name: "Me on Plurk",
-    icon: "plurk",
+    name: "Plurk",
     href: "https://www.plurk.com/YtCharmyngeBole",
   },
   {
-    name: "Me on GitHub",
-    icon: "github",
+    name: "GitHub",
     href: "https://github.com/YtCharmyngeBole",
   },
 ];
