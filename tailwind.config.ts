8ffa572b301243cbd331e81a144aced38c4a1d99
That Charming BOLE <165321522+YtCharmyngeBole@users.noreply.github.com>
2024-09-19 11:21:54 +0700

    Fix hover/focus colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ import { Oklch, rem, round } from "#lib/styling.ts";
 /**
  * Custom component in Tailwind
  */
-const customComponent: PluginCreator = ({ addComponents }) => {
+const customComponent: PluginCreator = ({ addComponents, addVariant }) => {
   addComponents({
     ".icon-masked": {
       display: "inline-block",
@@ -27,6 +27,15 @@ const customComponent: PluginCreator = ({ addComponents }) => {
       fontStyle: "oblique",
     },
   });
+  addVariant("hocus", ["&:hover", "&:focus"]);
+  addVariant("group-hocus", [
+    ":merge(.group):hover &",
+    ":merge(.group):focus &",
+  ]);
+  addVariant("peer-hocus", [
+    ":merge(.peer):hover ~ &",
+    ":merge(.peer):focus ~ &",
+  ]);
 };
 
 export default {
@@ -47,85 +56,12 @@ export default {
         touch: "oklch(90.06% 0.0373 85.99)",
         muted: "oklch(53.41% 0.0434 313.07)",
         ground: "oklch(70.77% 0.1844 27.61)",
-        excited: "oklch(89.67% 0.079 88.89)",
+        excited: "oklch(94.55% 0.0661 88.89)",
         beetroot: "oklch(65.17% 0.103 360)",
         orange: "oklch(70.96% 0.147 25.44)",
         lemon: "oklch(85.52% 0.107 88.56)",
         eggplant: "oklch(69.12% 0.126 314.07)",
       },
-      // grayext: {
-      //   325: Oklch.interpolate(0.25, gray300, gray400).toString(),
-      //   350: Oklch.interpolate(0.5, gray300, gray400).toString(),
-      //   375: Oklch.interpolate(0.75, gray300, gray400).toString(),
-      //   425: Oklch.interpolate(0.25, gray400, gray500).toString(),
-      //   450: Oklch.interpolate(0.5, gray400, gray500).toString(),
-      //   475: Oklch.interpolate(0.75, gray400, gray500).toString(),
-      // },
-      // beetroot: {
-      //   50: new Oklch(0.9723, 0.007, 354.79).toString(),
-      //   100: new Oklch(0.9473, 0.014, 0.27).toString(),
-      //   200: new Oklch(0.8928, 0.03, 356.35).toString(),
-      //   300: new Oklch(0.8295, 0.047, 357.76).toString(),
-      //   400: new Oklch(0.7488, 0.072, 358.31).toString(),
-      //   500: new Oklch(0.6517, 0.103, 360).toString(),
-      //   600: new Oklch(0.5012, 0.119, 2.91).toString(),
-      //   700: new Oklch(0.4589, 0.109, 1.92).toString(),
-      //   800: new Oklch(0.3999, 0.091, 1.72).toString(),
-      //   900: new Oklch(0.3188, 0.068, 1.58).toString(),
-      //   950: new Oklch(0.2224, 0.043, 0.76).toString(),
-      // },
-      // eggplant: {
-      //   50: new Oklch(0.973, 0.011, 316.49).toString(),
-      //   100: new Oklch(0.9352, 0.024, 315.47).toString(),
-      //   200: new Oklch(0.8781, 0.048, 314.97).toString(),
-      //   300: new Oklch(0.8132, 0.075, 314.38).toString(),
-      //   400: new Oklch(0.7578, 0.097, 313.97).toString(),
-      //   500: new Oklch(0.6912, 0.126, 314.07).toString(),
-      //   600: new Oklch(0.5752, 0.173, 313.15).toString(),
-      //   700: new Oklch(0.4621, 0.157, 312.6).toString(),
-      //   800: new Oklch(0.3548, 0.114, 313.07).toString(),
-      //   900: new Oklch(0.2273, 0.063, 312.22).toString(),
-      //   950: new Oklch(0.1693, 0.036, 314.62).toString(),
-      // },
-      // fadecream: {
-      //   50: new Oklch(0.9769, 0.008, 48.66).toString(),
-      //   100: new Oklch(0.9627, 0.012, 59.57).toString(),
-      //   200: new Oklch(0.9166, 0.027, 53.14).toString(),
-      //   300: new Oklch(0.8716, 0.043, 54.36).toString(),
-      //   400: new Oklch(0.817, 0.062, 53.75).toString(),
-      //   500: new Oklch(0.7738, 0.078, 52.66).toString(),
-      //   600: new Oklch(0.7225, 0.097, 52.46).toString(),
-      //   700: new Oklch(0.6582, 0.122, 51.1).toString(),
-      //   800: new Oklch(0.5751, 0.12, 49.7).toString(),
-      //   900: new Oklch(0.4596, 0.092, 50.82).toString(),
-      //   950: new Oklch(0.3448, 0.066, 50.29).toString(),
-      // },
-      // lemon: {
-      //   50: new Oklch(0.9852, 0.011, 89.72).toString(),
-      //   100: new Oklch(0.9703, 0.023, 90.75).toString(),
-      //   200: new Oklch(0.9304, 0.052, 89.04).toString(),
-      //   300: new Oklch(0.8967, 0.079, 88.89).toString(),
-      //   400: new Oklch(0.8552, 0.107, 88.56).toString(),
-      //   500: new Oklch(0.8073, 0.134, 87.23).toString(),
-      //   600: new Oklch(0.7515, 0.147, 84.06).toString(),
-      //   700: new Oklch(0.6836, 0.133, 83.93).toString(),
-      //   800: new Oklch(0.6008, 0.116, 85.17).toString(),
-      //   900: new Oklch(0.462, 0.088, 84.99).toString(),
-      //   950: new Oklch(0.3712, 0.068, 86.19).toString(),
-      // },
-      // orange: {
-      //   50: new Oklch(0.9489, 0.021, 21.18).toString(),
-      //   100: new Oklch(0.9012, 0.043, 23.31).toString(),
-      //   200: new Oklch(0.8063, 0.091, 24.28).toString(),
-      //   300: new Oklch(0.7096, 0.147, 25.44).toString(),
-      //   400: new Oklch(0.6387, 0.194, 27.61).toString(),
-      //   500: new Oklch(0.5859, 0.22, 29.69).toString(),
-      //   600: new Oklch(0.4954, 0.184, 29.6).toString(),
-      //   700: new Oklch(0.4113, 0.15, 29.63).toString(),
-      //   800: new Oklch(0.3117, 0.109, 29.35).toString(),
-      //   900: new Oklch(0.2044, 0.063, 29.42).toString(),
-      //   950: new Oklch(0.1446, 0.038, 27.87).toString(),
-      // },
     },
     fontFamily: {
       display: ["Barlow", "'Barlow fallback'", ...defaultTheme.fontFamily.sans],
