73237cd3aec524c465a7b24f7c19fdfa72ad6dcd
That Charming BOLE <165321522+YtCharmyngeBole@users.noreply.github.com>
2025-07-04 19:10:42 +0700

    Add page number on print & Fix awkward page breaks

--- ec.config.mjs
+++ ec.config.mjs
@@ -19,6 +19,7 @@ const extraLanguages = glob
  * @type import('astro-expressive-code').AstroExpressiveCodeOptions
  */
 export default defineEcConfig({
+  // themes: ["catppuccin-mocha", "catppuccin-latte"],
   themes: ["catppuccin-mocha"],
   styleOverrides: {
     borderColor: "var(--ec-border-color)",
@@ -32,6 +33,7 @@ export default defineEcConfig({
       shadowColor: "var(--ec-frame-shadow-color)",
     },
   },
+  useDarkModeMediaQuery: false,
   shiki: {
     langs: extraLanguages,
   },
