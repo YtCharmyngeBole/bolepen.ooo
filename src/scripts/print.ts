53fde1ee1d51099e98b669f1cdd12b224a35c326
That Charming BOLE <165321522+YtCharmyngeBole@users.noreply.github.com>
2025-08-13 23:03:48 +0700

    Refactors and modernizes the media print events

--- src/scripts/print.ts
+++ src/scripts/print.ts
@@ -1,23 +1,32 @@
-type PrintEventCallback = () => void;
+let mediaPrintEventsInitialized = false;
 
-interface PrintEventListenerOptions {
-  onPrintStart?: PrintEventCallback;
-  onPrintEnd?: PrintEventCallback;
+/**
+ * Initializes a pair of custom events describing when the "print" media query
+ * is attached to or detached from the "window" global object.
+ * - The "mediaprintstart" event is dispatched when the user
+ *   toggles the print preview or is about to print the page.
+ * - The "mediaprintend" event is dispatched when the user
+ *   untoggles the print preview or is done printing the page.
+ */
+export function initMediaPrintEvents(): void {
+  if (mediaPrintEventsInitialized) return;
+  if (!window.matchMedia) return;
+
+  const mediaQueryList: MediaQueryList = window.matchMedia("print");
+  mediaQueryList.addEventListener("change", (mql: MediaQueryListEvent) => {
+    if (mql.matches) {
+      window.dispatchEvent(new CustomEvent("mediaprintstart"));
+    } else {
+      window.dispatchEvent(new CustomEvent("mediaprintend"));
+    }
+  });
+
+  mediaPrintEventsInitialized = true;
 }
 
-export function addPrintEventListener(
-  options: PrintEventListenerOptions = {},
-): void {
-  if (window.matchMedia) {
-    const mediaQueryList: MediaQueryList = window.matchMedia("print");
-    mediaQueryList.addEventListener("change", (mql: MediaQueryListEvent) => {
-      if (mql.matches) {
-        // The document is being printed
-        options.onPrintStart?.();
-      } else {
-        // The document is no longer being printed
-        options.onPrintEnd?.();
-      }
-    });
-  }
+// Attempts to auto-register the custom events
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", initMediaPrintEvents);
+} else {
+  initMediaPrintEvents();
 }
